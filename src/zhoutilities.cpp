8b398e9a857aed1f9d6d81ba55e779c7e4edf528
laisuk <laisuk@yahoo.com>
2025-06-04 13:28:42 +0800

    Move OpenCC to constructor

--- src/zhoutilities.cpp
+++ src/zhoutilities.cpp
@@ -26,7 +26,7 @@ size_t find_max_utf8_length(const std::string_view sv, size_t max_byte_count) {
 }
 
 std::string convert_punctuation(const std::string_view sv, const std::string_view config) {
-    std::unordered_map<std::wstring, std::wstring> s2t_punctuation_chars = {
+    const std::unordered_map<std::wstring, std::wstring> s2t_mapping = {
         // Declare a dictionary to store the characters and their mappings
         {L"“", L"「"},
         {L"”", L"」"},
@@ -34,38 +34,38 @@ std::string convert_punctuation(const std::string_view sv, const std::string_vie
         {L"’", L"』"}
     };
 
+    const std::unordered_map<std::wstring, std::wstring> t2s_mapping = {
+        // Declare a dictionary to store the characters and their mappings
+        {L"「", L"“"},
+        {L"」", L"”"},
+        {L"『", L"‘"},
+        {L"』", L"’"}
+    };
+
     const std::wstring input_text = utf8_to_wstring(sv.data());
-    std::wstring output_text;
+
+    std::wstring pattern = L"[";
+    std::unordered_map<std::wstring, std::wstring> mapping;
     // Use the join method to create the regular expression patterns
     if (config.substr(0, 1) == "s") {
         // Create the regular expression patterns
-        std::wstring s2tPattern = L"[";
-        for (const auto &pair: s2t_punctuation_chars) {
-            s2tPattern += pair.first;
+        for (const auto &[key, val]: s2t_mapping) {
+            pattern += key;
         }
-        s2tPattern += L"]";
-        const boost::wregex s2t_regex(s2tPattern);
-        output_text = boost::regex_replace(input_text, s2t_regex,
-                                           [&](const boost::wsmatch &m) {
-                                               return s2t_punctuation_chars[m.str(0)];
-                                           });
+        mapping = s2t_mapping;
     } else {
-        // Use the map to reverse the dictionary
-        std::unordered_map<std::wstring, std::wstring> t2s_punctuation_chars;
-        for (const auto &pair: s2t_punctuation_chars) {
-            t2s_punctuation_chars[pair.second] = pair.first;
+        for (const auto &[key, val]: t2s_mapping) {
+            pattern += key;
         }
-        std::wstring t2sPattern(L"[");
-        for (const auto &pair: t2s_punctuation_chars) {
-            t2sPattern += pair.first;
-        }
-        t2sPattern += L"]";
-        const boost::wregex t2sRegex(t2sPattern);
-        output_text = boost::regex_replace(input_text, t2sRegex,
-                                           [&](const boost::wsmatch &m) {
-                                               return t2s_punctuation_chars[m.str(0)];
-                                           });
+        mapping = t2s_mapping;
     }
+    pattern += L"]";
+
+    const boost::wregex regex(pattern);
+    const std::wstring output_text = boost::regex_replace(input_text, regex,
+                                                          [&](const boost::wsmatch &m) {
+                                                              return mapping[m.str(0)];
+                                                          });
     return wstring_to_utf8(output_text);
 }
 
